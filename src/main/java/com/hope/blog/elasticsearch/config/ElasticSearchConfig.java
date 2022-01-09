dd6ab9cbd22dcb6764aa10c3f6ebe00e460d203f
augetys <1181881941@qq.com>
2022-01-09 10:15:23 +0800

    优化

--- src/main/java/com/hope/blog/elasticsearch/config/ElasticSearchConfig.java
+++ src/main/java/com/hope/blog/elasticsearch/config/ElasticSearchConfig.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 
+
 /**
  * Created by lijin on  2021/9/14
  */
 @Configuration
 public class ElasticSearchConfig {
-//    @Bean
+    //    @Bean
 //    public RestHighLevelClient restHighLevelClient() {
 //        return new RestHighLevelClient(
 //                RestClient.builder(
@@ -19,17 +20,20 @@ public class ElasticSearchConfig {
 //                )
 //        );
 //    }
+    private final long MAX_KEEP_ALIVE_MINUTES = 1;
 
     /**
      * springboot 整合 spring data elasticsearch 打印查询语句
      * https://blog.csdn.net/weixin_43549350/article/details/116322865
+     * https://www.codetd.com/article/12139074
      * @return
      */
     @Bean(destroyMethod = "close")
     public RestHighLevelClient restClient() {
-
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("110.42.244.186:9200")
+                .withConnectTimeout(10000)
+                .withSocketTimeout(60000)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
